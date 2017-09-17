44ed9452dbc0538319fe3952d47e8e0c4702fc97
Aaron <aaron_oh@163.com>
2017-09-17 19:01:13 +0800

    Initial commit

---
+++ app/src/main/java/me/aaron/top250/activities/InfoActivity.java
@@ -0,0 +1,15 @@
+package me.aaron.top250.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.aaron.top250.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
